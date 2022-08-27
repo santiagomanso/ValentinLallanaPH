2b58943bd0dbfd06ed9b195d8f0df11152f3c721
santiagomanso <santiagomanso@web.de>
2022-08-27 22:19:38 +0200

    🔨 added modal, swiper

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-import "./Card.css";
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import './Card.css'
 
-const Card = ({emoji, heading, detail, color}) => {
+const Card = ({ emoji, heading, detail, color }) => {
+  //here i check for each type of event and i assign a className to futher put img css as cover
+
   return (
-    <div className="card" style={{borderColor: {color}}}> 
-      <img src={emoji} alt="" />
-      <span>{heading}</span>
-      <span>{detail}</span>
-      <button className="c-button">LEARN MORE</button>
+    <div className='card' style={{ borderColor: { color } }}>
+      <div>
+        <span>{heading}</span>
+        <span>{detail}</span>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
